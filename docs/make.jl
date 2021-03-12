62e0cc29301ec43b3381fa0cf5b2b2055bbfc5f7
Tim Lucas Sabelmann <tim.sabelmann@hotmail.de>
2021-03-12 12:48:10 +0100

    init: Added docs directory

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using CANTools
+using Documenter
+
+DocMeta.setdocmeta!(CANTools, :DocTestSetup, :(using CANTools); recursive=true)
+
+makedocs(;
+    modules=[CANTools],
+    authors="Tim Lucas Sabelmann",
+    repo="https://github.com/tsabelmann/CANTools.jl/blob/{commit}{path}#{line}",
+    sitename="CANTools.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://tsabelmann.github.io/CANTools.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/tsabelmann/CANTools.jl",
+)
