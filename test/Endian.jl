0ce39995be6981837b665e018da80be4beeea740
Tim Lucas Sabelmann <tim.sabelmann@hotmail.de>
2021-03-17 18:23:48 +0100

    refact: Changed test cases for Endian checks

--- test/Endian.jl
+++ test/Endian.jl
@@ -1,18 +1,18 @@
-using CANTools
+using CANTools.Utils
 using Test
 
 @testset "Endian" begin
     @testset "is_little_or_big_endian" begin
-        is_little_endian = CANTools.is_little_endian()
-        is_big_endian = CANTools.is_big_endian()
+        is_little = is_little_endian()
+        is_big = is_big_endian()
 
-        @test (is_little_endian || is_big_endian) == true
+        @test (is_little || is_big) == true
     end
 
     @testset "is_little_and_big_endian" begin
-        is_little_endian = CANTools.is_little_endian()
-        is_big_endian = CANTools.is_big_endian()
+        is_little = is_little_endian()
+        is_big = is_big_endian()
 
-        @test (is_little_endian && is_big_endian) == false
+        @test (is_little && is_big) == false
     end
 end
