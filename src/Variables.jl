4f85a1fa7eaaf1a1d4b5dc306d9c72faaa6a10c5
Tim Lucas Sabelmann <tim.sabelmann@hotmail.de>
2021-03-25 13:57:27 +0100

    feat: Added include for variables.jl

--- src/Variables.jl
+++ src/Variables.jl
@@ -1,17 +1,5 @@
 module Variables
-
-    abstract type AbstractVariable end
-
-    # abstract type NamedVariable <: AbstractVariable end
-    #
-    # abstract type UnnamedVariable <: AbstractVariable end
-    #
-    # abstract type Variable <: UnnamedVariable end
-    #
-    # abstract type MultiplexerVariable
-    #
-    # abstract type IntegerVariable <: UnnamedVariable end
-    #
-    # abstract type FloatingVariable <: UnnamedVariable end
+    
+    include("variables.jl")
 
 end
