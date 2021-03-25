42c318c9d73bf870ba57a533c4399aa38748228c
Tim Lucas Sabelmann <tim.sabelmann@hotmail.de>
2021-03-25 13:57:47 +0100

    feat: Added types

---
+++ src/variables.jl
@@ -0,0 +1,13 @@
+abstract type AbstractVariable end
+
+# abstract type NamedVariable <: AbstractVariable end
+#
+# abstract type UnnamedVariable <: AbstractVariable end
+#
+# abstract type Variable <: UnnamedVariable end
+#
+# abstract type MultiplexerVariable
+#
+# abstract type IntegerVariable <: UnnamedVariable end
+#
+# abstract type FloatingVariable <: UnnamedVariable end
