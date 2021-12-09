f2f2b1c8807112376b5e30338c2e9e1b8589f1ce
tsabelmann <tim.sabelmann@hotmail.de>
2021-12-09 17:52:04 +0100

    docs: Added documentation

--- src/Endian.jl
+++ src/Endian.jl
@@ -1,4 +1,17 @@
-function is_little_endian()
+"""
+    is_little_endian()    
+
+    Returns if the system has little-endian byte-order.
+
+```jldoctest
+is_little_endian()
+
+# output
+
+false
+```
+"""
+function is_little_endian()::Bool
     x::UInt16 = 0x0001
     lst = reinterpret(UInt8, [x])
 
@@ -9,6 +22,6 @@ function is_little_endian()
     end
 end
 
-function is_big_endian()
+function is_big_endian()::Bool
     return !is_little_endian()
 end
