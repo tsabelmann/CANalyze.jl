d3ce8498df53fab41ec94ed57b365deee4d9f524
tsabelmann <tim.sabelmann@hotmail.de>
2021-12-10 20:51:01 +0100

    docs: Added documentation

--- src/convert.jl
+++ src/convert.jl
@@ -1,12 +1,32 @@
 """
+    to_bytes(num::Number) -> Vector{UInt8}
+
+Creates the byte array consistituting the number `num`
+
+# Arguments
+- `num::Number`: the type of the mask
+
+# Returns
+- `Vector{UInt8}`: the bytes constituting the number `num`
 """
 function to_bytes(num::Number)::Vector{UInt8}
     return reinterpret(UInt8, [num])
 end
 
 """
+    from_bytes(type::Type{T}, array::A) where {A <: AbstractArray{UInt8}, T <: Number} -> T
+
+Creates a value of type `T` constituted by the byte-array `array`
+
+# Arguments
+- `type::Type{T}`: the type to which the byte-array is transformed
+- `array::A`: the byte array
+
+# Returns
+- `T`:
 """
-function from_bytes(array::A, type::Type{T}) where {A <: AbstractArray{UInt8}, T <: Number}
+function from_bytes(type::Type{T}, array::A)::T where
+    {A <: AbstractArray{UInt8}, T <: Number}
     values = reinterpret(type, array)
     return values[1]
 end
