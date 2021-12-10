dc8012f11bb14ffae3a2869b131a46b88364f6b0
tsabelmann <tim.sabelmann@hotmail.de>
2021-12-10 19:51:42 +0100

    docs: Added documentation

--- src/mask.jl
+++ src/mask.jl
@@ -1,25 +1,56 @@
 """
+    mask(::Type{T}, length::UInt8, shift::UInt8) where {T <: Unsigned} -> T
 
+Creates a mask of type `T` with `length` number of bits and right-shifted by `shift` number
+of bits
+
+# Arguments
+- `Type{T}`: the type of the mask
+- `length::UInt8`: the number of bits
+- `shift::UInt8`: the right-shift
+
+# Returns
+- `T`: the mask defined by `length` and `shift`
 """
-function mask(::Type{T}, length::UInt8, shift::UInt8) where {T <: Unsigned}
+function mask(::Type{T}, length::UInt8, shift::UInt8)::T where {T <: Unsigned}
     ret = mask(T, length)
     ret <<= shift
     return T(ret)
 end
 
 """
-"""
-function mask(::Type{T}, length::S1, shift::S2) where
-    {T <: Unsigned, S1 <: Integer, S2 <: Integer}
+    mask(::Type{T}, length::Integer, shift::Integer) where {T <: Unsigned} -> T
 
+Creates a mask of type `T` with `length` number of bits and right-shifted by `shift` number
+of bits
+
+# Arguments
+- `Type{T}`: the type of the mask
+- `length::Integer`: the number of bits
+- `shift::Integer`: the right-shift
+
+# Returns
+- `T`: the mask defined by `length` and `shift`
+"""
+function mask(::Type{T}, length::Integer, shift::Integer)::T where {T <: Unsigned}
     l = convert(UInt8, length)
     s = convert(UInt8, shift)
     return mask(T, l, s)
 end
 
 """
+    mask(::Type{T}, length::UInt8) where {T <: Unsigned} -> T
+
+Creates a mask of type `T` with `length` number of bits
+
+# Arguments
+- `Type{T}`: the type of the mask
+- `length::UInt8`: the number of bits
+
+# Returns
+- `T`: the mask defined by `length`
 """
-function mask(::Type{T}, length::UInt8) where {T <: Unsigned}
+function mask(::Type{T}, length::UInt8)::T where {T <: Unsigned}
     ret = zero(T)
     if length > 0
         for i in 1:(length-1)
@@ -32,19 +63,49 @@ function mask(::Type{T}, length::UInt8) where {T <: Unsigned}
 end
 
 """
+    mask(::Type{T}, length::Integer) where {T <: Unsigned} -> T
+
+Creates a mask of type `T` with `length` number of bits
+
+# Arguments
+- `Type{T}`: the type of the mask
+- `length::Integer`: the number of bits
+
+# Returns
+- `T`: the mask defined by `length`
 """
-function mask(::Type{T}, length::S) where {T <: Unsigned, S <: Integer}
+function mask(::Type{T}, length::Integer)::T where {T <: Unsigned}
     l = convert(UInt8, length)
     return mask(T, l)
 end
 
-function mask(::Type{T}) where {T <: Unsigned}
+"""
+    mask(::Type{T}) where {T <: Unsigned} -> T
+
+Creates a full mask of type `T`
+
+# Arguments
+- `Type{T}`: the type of the mask
+
+# Returns
+- `T`: the full mask
+"""
+function mask(::Type{T})::T where {T <: Unsigned}
     return full_mask(T)
 end
 
 """
+    full_mask(::Type{T}) where {T <: Unsigned} -> T
+
+Creates a full mask of type `T`
+
+# Arguments
+- `Type{T}`: the type of the mask
+
+# Returns
+- `T`: the full mask
 """
-function full_mask(::Type{T}) where {T <: Unsigned}
+function full_mask(::Type{T})::T where {T <: Unsigned}
     ret = zero(T)
     for i in 0:(8sizeof(T) - 2)
         ret += 1
@@ -55,8 +116,16 @@ function full_mask(::Type{T}) where {T <: Unsigned}
 end
 
 """
-    zero
+    zero_mask(::Type{T}) where {T <: Unsigned} -> T
+
+Creates a zero mask of type `T`
+
+# Arguments
+- `Type{T}`: the type of the mask
+
+# Returns
+- `T`: the zero mask
 """
-function zero_mask(::Type{T}) where {T <: Unsigned}
+function zero_mask(::Type{T})::T where {T <: Unsigned}
     return zero(T)
 end
