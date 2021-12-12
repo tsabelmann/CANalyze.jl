386d14ab7e391129baf76557c3d729de424a8df2
tsabelmann <tim.sabelmann@hotmail.de>
2021-12-12 22:29:58 +0100

    refact: Removed exports statments

--- src/CANTools.jl
+++ src/CANTools.jl
@@ -1,8 +1,6 @@
 module CANTools
     include("Utils.jl")
     using .Utils
-    export is_little_endian, is_big_endian
-    export mask, zero_mask, full_mask
 
     include("Frames.jl")
     using .Frames
