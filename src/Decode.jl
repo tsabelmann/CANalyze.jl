ca2a77aa55a3e3ba605ab5ee43762e72a523008e
tsabelmann <tim.sabelmann@hotmail.de>
2021-12-21 21:45:12 +0100

    feat: Implemented decode for Signal partially

--- src/Decode.jl
+++ src/Decode.jl
@@ -40,9 +40,48 @@ module Decode
 
     """
     """
-    function decode(signal::Signal.Unsigned{T}, data::Frames.CANFrame) where {T}
-        
+    function decode(signal::Signals.Signal{T}, data::Frames.CANFrame) where {T}
+        byte_order = Signals.byte_order(signal)
+        signed = Signals.signed(signal)
+
+        if byte_order == :little_endian
+            start = Signals.start(signal)
+            length = Signals.length(signal)
+            factor = Signals.factor(signal)
+            offset = Signals.offset(signal)
+
+            if start >= 8*Frames.dlc(data)
+                throw(DomainError())
+            end
+
+            if start + length - 1 >= 8 * Frames.dlc(data)
+                throw(DomainError())
+            end
+
+            if signed
+                value = Utils.from_bytes(Int64, Frames.data(data))
+            else
+                value = Utils.from_bytes(UInt64, Frames.data(data))
+            end
+
+            value = value >> start
+
+            if signed
+                value = value & Utils.mask(Int64, length)
+                # sign extend value if most-significant bit is 1
+                if (value >> (length - 1)) & 0x01 != 0
+                    value = value + ~Utils.mask(Int64, length)
+                end
+            else
+                value = value & Utils.mask(UInt64, length)
+            end
+
+            result = factor * value + offset
+            return result
+        elseif byte_order == :big_endian
+
+        else
+            throw(DomainError(byte_order, "Byte order not supported"))
+        end
     end
-
-
 end
