f5bff1a0fb5c92b188e5255bf71c64b6d17cddd1
tsabelmann <tim.sabelmann@hotmail.de>
2021-12-22 10:45:03 +0100

    feat: Changed example

--- examples/signal_1.jl
+++ examples/signal_1.jl
@@ -3,7 +3,7 @@ using CANTools.Frames
 using CANTools.Signals
 
 frame = Frames.CANFrame(20, [1, 2, 0xFD, 4, 5, 6, 7, 8])
-signal = Signals.Signal(34, 17, 1.0, 0.0; signed=true)
+signal = Signals.Signal(start=34, length=17, factor=1.0, offset=0.0; signed=true, byte_order=:big_endian)
 
 value = Decode.decode(signal,frame)
 println(value)
