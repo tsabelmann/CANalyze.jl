d25bf12e4721c4359653998b2a06b530662c21a6
tsabelmann <tim.sabelmann@hotmail.de>
2022-01-02 23:58:55 +0100

    feat: Added tests testing equality

--- test/Frames.jl
+++ test/Frames.jl
@@ -1,1 +1,12 @@
+using CANalyze.Frames
+using Test
+
+
 @info "CANalyze.Frames tests..."
+@testset "frames" begin
+    @testset "equal" begin
+        frame1 = CANFrame(0x14, []; is_extended=true)
+        frame2 = CANFrame(0x14; is_extended=true)
+        @test frame1 == frame2
+    end
+end
