7b3f74c560407378f63571e799419e673ec76905
tsabelmann <tim.sabelmann@hotmail.de>
2022-01-07 07:56:27 +0100

    feat: Added tests for raw signals

--- test/Decode.jl
+++ test/Decode.jl
@@ -89,3 +89,54 @@ end
         end
     end
 end
+
+
+@testset "raw" begin
+    @testset "raw_1" begin
+        for start=0:63
+            for len=1:(64-start)
+                m = Utils.mask(UInt64, len, start)
+                signal = Signals.Raw(start=start, length=len, byte_order=:little_endian)
+                frame = Frames.CANFrame(0x1FF, Utils.to_bytes(m))
+                decode = Decode.decode(signal, frame)
+                value = Utils.mask(UInt64, len)
+                @test decode == value
+            end
+        end
+    end
+
+    @testset "raw_2" begin
+        signal = Signals.Raw(start=7, length=8, byte_order=:big_endian)
+        frame = Frames.CANFrame(0x1FF, [i for i=1:8])
+        decode = Decode.decode(signal, frame)
+        @test decode == 1
+    end
+
+    @testset "raw_3" begin
+        signal = Signals.Raw(start=7, length=16, byte_order=:big_endian)
+        frame = Frames.CANFrame(0x1FF, 0xAB, 0xCD)
+        decode = Decode.decode(signal, frame)
+        @test decode == 0xABCD
+    end
+
+    @testset "raw_4" begin
+        signal = Signals.Raw(start=7, length=24, byte_order=:big_endian)
+        frame = Frames.CANFrame(0x1FF, 0xAB, 0xCD, 0xEF)
+        decode = Decode.decode(signal, frame)
+        @test decode == 0xABCDEF
+    end
+
+    @testset "raw_5" begin
+        signal = Signals.Raw(start=7, length=64, byte_order=:big_endian)
+        frame = Frames.CANFrame(0x1FF, [i for i=1:8])
+        decode = Decode.decode(signal, frame)
+        @test decode == 0x01_02_03_04_05_06_07_08
+    end
+
+    @testset "raw_6" begin
+        signal = Signals.Raw(start=3, length=8, byte_order=:big_endian)
+        frame = Frames.CANFrame(0x1FF, 0x21, 0xAB)
+        decode = Decode.decode(signal, frame)
+        @test decode == 0x1A
+    end
+end
